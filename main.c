70791853ae0b28c6e0c88673e8032bd1774ffed5
TobbeL <tobbeman1994@gmail.com>
2017-04-05 16:02:10 +0200

    test2

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "rs232.h"
 
-#define COMPORT         5       // this is '/dev/ttyUSB0' (for arduino) or choose wherever you added '/dev/ttyAMA0' (for raspberry pi UART pins) to the list
-#define BAUDRATE      9600   // or whatever baudrate you want
+#define COMPORT         16       // this is '/dev/ttyUSB0' (for arduino) or choose wherever you added '/dev/ttyAMA0' (for raspberry pi UART pins) to the list
+#define BAUDRATE      57600   // or whatever baudrate you want
 #define RECEIVE_CHARS   8      // or whatever size of buffer you want to receive
 #define SEND_CHARS      1      // or whatever size of buffer you want to send
 
@@ -14,8 +14,13 @@ int main (int argc, char **argv) {
    while(1) {
       // change send_byte and/or send_buffer with what you want to send. Then:
       //RS232_SendByte(COMPORT, send_byte); // or:
-      RS232_SendBuf(COMPORT, send_buffer, SEND_CHARS);
 
+      RS232_SendBuf(COMPORT, send_buffer, SEND_CHARS);
+      int o = 10000;
+      while( o > 1){
+        o--;
+      }
+      RS232_SendBuf(COMPORT, "o", SEND_CHARS);
       // and/or:
       //RS232_PollComport(COMPORT, receive_buffer, RECEIVE_CHARS);
 
