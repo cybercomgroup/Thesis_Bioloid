de90f47487a827eefab88faa4b8f638dbf819e30
TobbeL <tobbeman1994@gmail.com>
2017-05-02 14:27:05 +0200

    Got simple request working

---
+++ audio/googlescript.sh
@@ -0,0 +1,2 @@
+curl -s -k -H "Content-Type: application/json" -H "Authorization: Bearer ya29.Elo-BEEGpxzEkne5OeM6frBfZ2XACgdiLaW-BsA7MteP9f9UfqUsuOfhzXUGlG4YIOUraFilz1ijRDskoMwc5-XlENpGX137LJxUNTnRICHkg8yzCa_tFRhVVvM
+" https://speech.googleapis.com/v1/speech:recognize -d @sync-request.json
