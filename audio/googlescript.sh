66d9e5f884756480791ea65a56230bf2b7620110
TobbeL <tobbeman1994@gmail.com>
2017-05-03 13:09:35 +0200

    Testscript for google api

--- audio/googlescript.sh
+++ audio/googlescript.sh
@@ -1,2 +1,1 @@
-curl -s -k -H "Content-Type: application/json" -H "Authorization: Bearer ya29.Elo-BEEGpxzEkne5OeM6frBfZ2XACgdiLaW-BsA7MteP9f9UfqUsuOfhzXUGlG4YIOUraFilz1ijRDskoMwc5-XlENpGX137LJxUNTnRICHkg8yzCa_tFRhVVvM
-" https://speech.googleapis.com/v1/speech:recognize -d @sync-request.json
+curl -s -k -H "Content-Type: application/json" -H "Authorization: Bearer ya29.Elo-BIxvXbq5E_3AjaNdtxNJ6KAvPuIMkICxRPc5zWMWTARQyGBc3GBfBZeyKdSGe8YVUaWsW0Od9Ay37MOJ_VEdt4joUK_eoejWf3KncT2hbSlApTB0U8cRi7Y" https://speech.googleapis.com/v1/speech:recognize -d @sync-request.json
