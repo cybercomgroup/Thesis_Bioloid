6cc0fdf37c1aab77e70ddb65e5c8098e8f438dd9
TobbeL <tobbeman1994@gmail.com>
2017-05-04 14:48:04 +0200

    Voice command draft ready testing

--- audio/audio.h
+++ audio/audio.h
@@ -1,21 +1,11 @@
 #include <string>
 #include <iostream>
 
+
 class audio {
 public:
-  void audio_init(const std::string &vocePath="audio/lib", bool initSynthesis=false, bool initRecognition=true, const std::string &grammarPath="audio/grammar", const std::string &grammarName="control");
-
-  void audio_destroy();
-
-  void audio_synthesize(const std::string &message);
-
-  void audio_stopSynthesizing();
-
-  int audio_getRecognizerQueueSize();
-
-  std::string audio_popRecognizedString ();
-
-  void audio_setRecognizerEnabled (bool e);
-
-  bool audio_isRecognizerEnabled ();
+  int init();
+  void listen();
+  int interpetFile(FILE *fh);
+  void destroy();
 };
