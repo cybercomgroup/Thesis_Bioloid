03ba00efeba809fb4f33cafeb3825e38db6eebb8
TobbeL <tobbeman1994@gmail.com>
2017-05-09 16:34:56 +0200

    Tweaked audio main for better user understanding

--- audio/main.cpp
+++ audio/main.cpp
@@ -8,11 +8,20 @@ int main(int argc, char *argv[])
 {
   string s;
   int pos;
+  bool printM = true;
   audio_init("lib/2005.lm","lib/2005.dic");
   thread listen(audio_listenForCommand);
   while(1)
   {
+    if(printM)
+    {
+      cout<<"Listening.."<<endl;
+      printM = false;
+    }
     if(audio_getCommandsSize() > 0){
+      printM = true;
+      cout<<"Heard this:"<<endl;
+
       s="";
       s=audio_popCommand();
       cout<<"Popped command: "<<s<<endl;
