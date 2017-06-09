db5ad10122cf6a9fc157cf18662852e7921fd8c2
Sebastian Lind <lindse@student.chalmers.se>
2017-06-09 14:44:44 +0200

    cleaner code

--- main.cpp
+++ main.cpp
@@ -22,11 +22,11 @@ void demoVoice();
 bool demoTurn();
 //void demodTurn();
 void realDemoa();
-inline void delay(unsigned long ms);
-void testTimeDemo();
-void findThing();
+void demo();
+bool turnTo(string cascade);
+void avoidObstacle();
 void sendStop();
-void walkStright();
+void walkForward();
 
 //Parser and global belonging variables
 bool cParser(int argN, char *argv[]);
@@ -40,7 +40,6 @@ string cascadeFile = "face_cascade.xml";
 bool rotImage = false;
 bool cameraFeed = false;
 bool printDetectionTime = false;
-int demo = 1;
 int comport = 16; //pNUMBER
 int baudrate = 57600; //bNUMBER
 
@@ -59,114 +58,141 @@ int main (int argc, char *argv[]) {
 
   //Set up:
   if(!cParser(argc,argv))
-    return 0;
+  return 0;
   initAll(configFile);
   printSettings();
 
-
-
-  if(demo == 1)
-//testTimeDemo();
- demoImage();
-
-
-  if(demo == 2)
-    demoVoice();
-
-  if(demo == 3)
-//    mainDemo();
-
+  demo();
   return 0;
 }
-
-void demoImage()
+void demo()
 {
- realDemoa();
-// demoTurn();
-}
-
-void realDemoa(){
-	bool object = false;
-	if(RS232_OpenComport(comport, baudrate, "8N1") != 1){
-	//Get voice Command
-	//switch(command){}
-	do{
-	object = demoTurn();
-	sleep(3);
-
-	walkStright();
-
-	send_buffer[0] = 'u';
-	RS232_SendBuf(comport, send_buffer, SEND_CHARS);
-	sleep(3);
-
-	if(object == true){
-	send_buffer[0] = 'c';
-	RS232_SendBuf(comport, send_buffer, 1);
-	break;
-	}
-
-	findThing();
-
-
-
-}while(!object);
-
-	//RS232_SendBuf(comport, send_buffer, SEND_CHARS);
-}
-	RS232_CloseComport(comport);
-
-}
-
-
-void findThing(){
- send_buffer[0] = 's';
- RS232_SendBuf(comport, send_buffer, 1);
- sleep(2);
- send_buffer[0] = 'b';
- RS232_SendBuf(comport, send_buffer, 1); 
-sleep(2);
- int tmp = rand() % 2;
- cout<<"plz"<<endl;
-	if(tmp == 0)
-		send_buffer[0] = 'a';
-	else
-	  	send_buffer[0] = 'd';
-
-	RS232_SendBuf(comport, send_buffer, 1);
-
-	sleep(4);
-	send_buffer[0] = 'b';
-	RS232_SendBuf(comport, send_buffer, 1);
-
+  if(RS232_OpenComport(comport, baudrate, "8N1") != 1)
+  {
+    string s,c = "";
+    bool quit;
+    audio_init("audio/lib/en-us","audio/lib/2005.lm","audio/lib/2005.dic");
+    thread listen(audio_listen);
+    while(!quit)
+    {
+      if(audio_getCommandsSize() > -1)
+      {
+        s = "";
+        s = audio_popCommand();
+        cout<<"Popped command: "<<s<<endl;
+        c = "";
+        c = audio_parseCommand("ROBOT",s);
+        cout<<"Command: "<<c<<endl;
+        if(!c.compare("TURN LEFT"))
+        {
+          send_buffer[0] = 'a';
+          RS232_SendBuf(comport, send_buffer, SEND_CHARS);
+        }
+        else if(!c.compare("TURN RIGHT"))
+        {
+          send_buffer[0] = 'd';
+          RS232_SendBuf(comport, send_buffer, SEND_CHARS);
+        }
+        else if(!c.compare("STOP"))
+        {
+          send_buffer[0] = 'b';
+          RS232_SendBuf(comport, send_buffer, SEND_CHARS);
+        }
+        else if(!c.compare("FIND FACE"))
+        {
+          findObject("face_cascade.xml")
+        }
+      }
+    }
+    listen.join();
+    //audio_destroy();
+  }
 
 }
 
-void walkStright(){
-	unsigned char receive1[RECEIVE_CHARS];
 
-	send_buffer[0] = 'w';
-	RS232_SendBuf(comport, send_buffer, 1);
 
-	char tmp;
-	while(tmp != 'b'){
-	RS232_PollComport(comport, receive1, 1);
-	tmp = receive1[0];
-	cout<<tmp<<endl;
-	}
-	send_buffer[0] = 'b';
-	RS232_SendBuf(comport, send_buffer, 1);
-	sleep(3);
+void findObject(string cascade){
+  bool object = false;
+  if(RS232_OpenComport(comport, baudrate, "8N1") != 1){
+    do{
+      object = turnTo(cascade);
+      sleep(3);
+
+      walkForward();
+
+      send_buffer[0] = 'u';
+      RS232_SendBuf(comport, send_buffer, SEND_CHARS);
+      sleep(3);
+
+      if(object == true){
+        send_buffer[0] = 'c';
+        RS232_SendBuf(comport, send_buffer, 1);
+        break;
+      }
+
+      avoidObstacle();
+
+
+
+    }while(!object);
+
+    //RS232_SendBuf(comport, send_buffer, SEND_CHARS);
+  }
+  RS232_CloseComport(comport);
+
+}
+
+
+void avoidObstacle(){
+  send_buffer[0] = 's';
+  RS232_SendBuf(comport, send_buffer, 1);
+  sleep(2);
+  send_buffer[0] = 'b';
+  RS232_SendBuf(comport, send_buffer, 1);
+  sleep(2);
+  int tmp = rand() % 2;
+  cout<<"plz"<<endl;
+  if(tmp == 0)
+  send_buffer[0] = 'a';
+  else
+  send_buffer[0] = 'd';
+
+  RS232_SendBuf(comport, send_buffer, 1);
+
+  sleep(4);
+  send_buffer[0] = 'b';
+  RS232_SendBuf(comport, send_buffer, 1);
+
+
+}
+
+void walkForward()
+{
+  unsigned char receive1[RECEIVE_CHARS];
+
+  send_buffer[0] = 'w';
+  RS232_SendBuf(comport, send_buffer, 1);
+
+  char tmp;
+  while(tmp != 'b'){
+    RS232_PollComport(comport, receive1, 1);
+    tmp = receive1[0];
+    cout<<tmp<<endl;
+  }
+  send_buffer[0] = 'b';
+  RS232_SendBuf(comport, send_buffer, 1);
+  sleep(3);
 }
 
 void sendStop(){
-	send_buffer[0] = 'b';
-	RS232_SendBuf(comport, send_buffer, SEND_CHARS);
+  send_buffer[0] = 'b';
+  RS232_SendBuf(comport, send_buffer, SEND_CHARS);
 }
 
 //void walkToObject()
 
-bool demoTurn()
+bool turnTo(string cascade)
 {
   int ori = -1;
   bool found = false;
@@ -178,72 +204,67 @@ bool demoTurn()
   }
 
   cv::CascadeClassifier cascade;
-  cascade.load("image/cascades/face_cascade.xml");
+  cascade.load("image/cascades/" + cascade);
 
   Mat img;
 
- // if(RS232_OpenComport(comport, baudrate, "8N1") != 1)
-  //{
-    time_t end = time(NULL) +3 ;
-    while(time(NULL) <= end && !found)
-    {
-      cap >> img;
-      cv::flip(img,img,-1); // to flip the camera uncomment this
+  time_t end = time(NULL) +3 ;
+  while(time(NULL) <= end && !found)
+  {
+    cap >> img;
+    cv::flip(img,img,-1); // to flip the camera uncomment this
 
-      ori = image_whereIsCascade(img,cascade,false);
-      cout<<"Time right Now: "<<time(NULL)<< " Time we aim for: "<< end <<endl;
-      imshow("Image", img);
-      cv::waitKey(1);
-	switch(ori){
-		case 4:
-		cout<<"Left"<< endl;
-		send_buffer[0] = 'a';
-		end += 5;
-		break;
+    ori = image_whereIsCascade(img,cascade,false);
+    cout<<"Time right Now: "<<time(NULL)<< " Time we aim for: "<< end <<endl;
+    imshow("Image", img);
+    cv::waitKey(1);
+    switch(ori){
+      case 4:
+      cout<<"Left"<< endl;
+      send_buffer[0] = 'a';
+      end += 5;
+      break;
 
-		case 5:
-		cout<<"Middle"<<endl;
-		send_buffer[0] = 'b';
-		found = true;
-		break;
+      case 5:
+      cout<<"Middle"<<endl;
+      send_buffer[0] = 'b';
+      found = true;
+      break;
 
-		case 6:
-		cout<<"Right"<<endl;
-		send_buffer[0] = 'd';
-		end += 5;
-		break;
+      case 6:
+      cout<<"Right"<<endl;
+      send_buffer[0] = 'd';
+      end += 5;
+      break;
 
-		default:
-		cout<<"Out of sight"<<endl;
-		send_buffer[0] = 'a';
-		break;
-	}
+      default:
+      cout<<"Out of sight"<<endl;
+      send_buffer[0] = 'a';
+      break;
+    }
 
-     //   RS232_SendBuf(comport, send_buffer, 1);
-//	send_buffer[0] = 'b';
-	RS232_SendBuf(comport, send_buffer, 1);
-      }
-	send_buffer[0] = 'b';
-	RS232_SendBuf(comport, send_buffer,1);
-     // }
-    return found;
-    RS232_CloseComport(comport);
+    RS232_SendBuf(comport, send_buffer, 1);
+  }
+  send_buffer[0] = 'b';
+  RS232_SendBuf(comport, send_buffer,1);
+  return found;
+  RS232_CloseComport(comport);
 
 }
 void testTimeDemo(){
-RS232_OpenComport(comport, baudrate, "8N1");
-time_t end = time(NULL) +  18;
-	while(time(NULL) < end){
-	send_buffer[0] = 'd';
-	RS232_SendBuf(comport, send_buffer, 1);
-	send_buffer[0] = 'b';
-	RS232_SendBuf(comport, send_buffer, 1);
+  RS232_OpenComport(comport, baudrate, "8N1");
+  time_t end = time(NULL) +  18;
+  while(time(NULL) < end){
+    send_buffer[0] = 'd';
+    RS232_SendBuf(comport, send_buffer, 1);
+    send_buffer[0] = 'b';
+    RS232_SendBuf(comport, send_buffer, 1);
 
-}
+  }
 
-//	time_t end = time(NULL) +5;
-//	while(time(NULL) < end)
-//	 cout<<"time: "<< time(NULL)<< endl;
+  //	time_t end = time(NULL) +5;
+  //	while(time(NULL) < end)
+  //	 cout<<"time: "<< time(NULL)<< endl;
 }
 
 void demoVoice()
@@ -291,15 +312,15 @@ void demoVoice()
   }
 }
 inline void delay(unsigned long ms){
-	usleep(ms * 1000);
+  usleep(ms * 1000);
 }
 
 bool initConf(string file)
 {
   INIReader reader(file);
   if (reader.ParseError() < 0) {
-      std::cout << "Can't load 'test.ini'\n";
-      return false;
+    std::cout << "Can't load 'test.ini'\n";
+    return false;
   }
   //image
   cameraDevice = reader.GetInteger("image", "cameraDevice", cameraDevice);
@@ -328,22 +349,22 @@ bool cParser( int argc, char** argv )
     switch (opt)
     {
       case 'c':
-        configFile = atoi(optarg);
-        break;
+      configFile = atoi(optarg);
+      break;
       case 'd':
-        demo = atoi(optarg);
-        break;
+      demo = atoi(optarg);
+      break;
       case 'h':
-        cout<<"-c [arg] for setting config-file"<<endl;
-        cout<<"-d [arg] set what demo to use"<<endl;
+      cout<<"-c [arg] for setting config-file"<<endl;
+      cout<<"-d [arg] set what demo to use"<<endl;
 
-        cout<<"Available demos:"<<endl;
-        cout<<"1 - detectionTest"<<endl;
-        cout<<"2 - orientationTest"<<endl;
-        cout<<"3 - numDetectionsTest"<<endl;
-        cout<<endl;
-        return false;
-        break;
+      cout<<"Available demos:"<<endl;
+      cout<<"1 - detectionTest"<<endl;
+      cout<<"2 - orientationTest"<<endl;
+      cout<<"3 - numDetectionsTest"<<endl;
+      cout<<endl;
+      return false;
+      break;
     }
   }
   //printSettings();
